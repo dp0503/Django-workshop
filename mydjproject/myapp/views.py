8c24f550c517011e68e7f6f2c90d8f0dd3862043
devarsh. <127184552+dp0503@users.noreply.github.com>
2024-12-11 14:48:57 +0530

    Add files via upload

--- mydjproject/myapp/views.py
+++ mydjproject/myapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def homepage(request):
-    return render(request,"home.html")
-def aboutpage(request):
-    return render(request,'about.html')
-def conatactpage(request):
-    return render(request,'contact.html')
+
+def homepageview(request):
+    return render(request,'home.html')
+
+def formpageview(request):
+    return render(request,'form.html')
+
+def formpageprocess(request):
+    a = int(request.POST["txt1"])
+    b = int(request.POST["txt2"])
+    c = a + b
+    return render(request,"ans.html",{'mya':a,'myb':b,'sum':c})
